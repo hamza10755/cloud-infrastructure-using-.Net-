60e67552516ef8ae01878cc0244ab3a746d1c6b5
hamza10755 <202311944@students.asu.edu.jo>
2026-05-26 15:12:27 +0300

    fully functional authinticated login and regester

---
+++ cloud-infrastructure/Migrations/20260526120216_AddFullNameToDeveloper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace cloud_infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFullNameToDeveloper : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- cloud-infrastructure/Program.cs
+++ cloud-infrastructure/Program.cs
@@ -3,17 +3,18 @@ using Microsoft.EntityFrameworkCore;
 using cloud_infrastructure.Models; // This ensures it finds your Developer and DbContext classes
 
 var builder = WebApplication.CreateBuilder(args);
-
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 // 1. Register your Database Connection FIRST
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite("Data Source=cloudportal.db")); // This will create a local SQLite file for testing
 
 // 2. Register Identity NEXT
 builder.Services.AddDefaultIdentity<Developer>(options => options.SignIn.RequireConfirmedAccount = false)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+    .AddEntityFrameworkStores<ApplicationDbContext>();  
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -37,5 +38,7 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
+
+app.MapRazorPages();
 
 app.Run();
